d1cb3ef3ed9f461c98754a15e031fa8b66650216
lupinooo <andreealupino@gmail.com>
2020-02-24 13:02:35 +0200

    first

---
+++ seminar2/src/net/ikmac/blog/loadimplementors/animals/Bear.java
@@ -0,0 +1,14 @@
+package net.ikmac.blog.loadimplementors.animals;
+
+public class Bear {
+    public Bear() {
+    	
+    }
+
+	@Override
+	public String toString() {
+		return "This is bear";
+	}
+    
+    
+}
---
+++ seminar2/src/net/ikmac/blog/loadimplementors/animals/Wolf.java
@@ -0,0 +1,20 @@
+package net.ikmac.blog.loadimplementors.animals;
+
+public class Wolf {
+   private final int wolf_speed=30;
+   
+   public Wolf() {
+   }
+
+@Override
+public String toString() {
+	// TODO Auto-generated method stub
+	return "This is a wolf";
+}
+
+public int getSpeed() {
+	return wolf_speed;
+}
+
+   
+}
